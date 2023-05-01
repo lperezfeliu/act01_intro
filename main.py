ac4f3755cd51588b2157d4d790ecb5428f42d755
lperezfeliu <132305334+lperezfeliu@users.noreply.github.com>
2023-05-01 22:38:30 +0100

    Actualizar main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+counter = 0
+
 def on_button_pressed_a():
     global counter
     counter += 1
     basic.show_number(counter)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_sound_loud():
+    pass
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+
 def on_button_pressed_b():
     global counter
     counter += -1
     basic.show_number(counter)
 input.on_button_pressed(Button.B, on_button_pressed_b)
-
-counter = 0
-counter = 0
--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.showNumber(counter)
+})
 input.onButtonPressed(Button.A, function () {
     counter += 1
     basic.showNumber(counter)
 })
-input.onSound(DetectedSound.Loud, function () {
-	
+input.onGesture(Gesture.Shake, function () {
+    counter = 0
+    basic.showNumber(counter)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+})
+input.onButtonPressed(Button.AB, function () {
+    counter = 0
+    basic.showNumber(counter)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 54, 54, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
-    counter += -1
-    basic.showNumber(counter)
+    if (counter > 0) {
+        counter += -1
+        basic.showNumber(counter)
+    } else {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    }
 })
 let counter = 0
-counter = 0
+basic.showNumber(counter)
